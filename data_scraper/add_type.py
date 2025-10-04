d4a8abce8a486bd6d34d93bcaeb0ece91b9e2af1
Sarthak Talukdar <sarthaktalukdar@runcodsprc219ld.rad.rutgers.edu>
2025-10-04 14:53:16 -0400

    HELP

---
+++ data_scraper/add_type.py
@@ -0,0 +1,19 @@
+import json 
+filename = "file.json"
+
+with open(filename, 'r') as file:
+    # Load the JSON data into a Python dictionary
+    data = json.load(file)
+
+categories = ["Robbery","Arson","Simple Assualt","Aggravated Assault","Murder","Burgulary","Shooting"]
+ans = []
+for i in range(len(data)):
+    my_dict = data[i]
+    for category in categories:
+        check = my_dict.get("type")
+        if category in check or category.upper() in check:
+            my_dict["category"]=category
+            ans.append(my_dict)
+            break 
+with open("new.json",'w') as file:
+    json.dump(ans,file,indent=4)
